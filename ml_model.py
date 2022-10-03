cba474f4fe63a6051e1ea095d3e4b00a3cde432f
vikas6050 <vikas6050@gmail.com>
2022-10-03 21:55:37 +0530

    Added 3 ml model

--- ml_model.py
+++ ml_model.py
@@ -1,3 +1,14 @@
 
 
 print("adding all ml related code here")
+
+def model1():
+    print("adding code for model1")
+    
+    
+def model2():
+    print("adding code for model2")
+    
+    
+def model3():
+    print("adding code for model3")        
